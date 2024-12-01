a26e8de143d769c0f9674fa3e286eb63fdfe1663
Gokul Krishna L J <35284617+GokulKrishna123@users.noreply.github.com>
2024-12-01 21:02:17 +0530

    Update alertDemo.py

--- Class_Codes/DAY_23&24 Selenium/alertDemo.py
+++ Class_Codes/DAY_23&24 Selenium/alertDemo.py
@@ -13,7 +13,7 @@ driver = webdriver.Firefox(service=service)
 driver.get("https://demo.guru99.com/test/delete_customer.php")
 
 searchbox= driver.find_element(By.NAME,"cusid")
-searchbox.send_keys("MuraleedharanA")
+searchbox.send_keys("Gokul Krishna")
 searchbox.send_keys(Keys.RETURN)
 
 alert =driver.switch_to.alert
